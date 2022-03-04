16138c188a70bb385d247e375201bd54d850479e
이승원 <squareo073@gmail.com>
2022-03-04 09:01:41 +0900

    Initial commit

---
+++ src/main/java/com/example/nhl01/Nhl01Application.java
@@ -0,0 +1,13 @@
+package com.example.nhl01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nhl01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Nhl01Application.class, args);
+    }
+
+}
